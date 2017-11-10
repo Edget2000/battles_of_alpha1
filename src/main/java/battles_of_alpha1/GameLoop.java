8455e18138c594c84b0a835f8b56e5f4d8dc1ff9
Anthony Edge <edget2000@yahoo.co.uk>
2017-11-10 14:02:50 +0000

    Starting Point....

---
+++ src/main/java/battles_of_alpha1/GameLoop.java
@@ -0,0 +1,14 @@
+package battles_of_alpha1;
+
+public class GameLoop {
+
+	public GameLoop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
